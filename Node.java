52da8800a6403b8160b0362aec28303d7f185c39
Carolina Romanholo <72838710+carolromanholo@users.noreply.github.com>
2023-02-10 20:33:12 -0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,23 @@
+public class Node {
+    private String valor;
+    private Node proximo;
+
+
+
+    public void setproximo(Node proximo) {
+        this.proximo = proximo;
+    }
+
+    public Node getproximo() {
+        return proximo;
+    }
+
+
+    public void setvalor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getvalor() {
+        return valor;
+    }
+}
